7a4efb02fe9d38efaa37af3f489797db7e8bdee6
Judah Matende <matendejudah@gmail.com>
2023-12-12 15:13:03 +0300

    add set up

---
+++ setup.bat
@@ -0,0 +1,8 @@
+go get -u github.com/joho/godotenv
+go get -u  "github.com/kataras/iris/v12"
+
+go install github.com/joho/godotenv
+go install  "github.com/kataras/iris/v12"
+
+mkdir ./build
+mkdir ./logs/daylog
